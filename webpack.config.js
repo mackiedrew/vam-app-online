d83598c1d25e29060e8eeadeb0423f76c5233bb9
Mackenzie Drew <mackiedrew@gmail.com>
2017-04-06 00:40:16 -0400

    :fire: Removing electron integration

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // Constants
 const PORT = 7447
-const BUILD_DIRECTORY = resolve(__dirname, 'build')
-const APP_DIRECTORY = resolve(__dirname, 'src')
+const BASE_DIRECTORY = resolve(__dirname)
+const BUILD_DIRECTORY = `${BASE_DIRECTORY}/build`
+const APP_DIRECTORY = `${BASE_DIRECTORY}/src`
 
 // Webpack plugins
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/src/index.ejs',
+  template: `${APP_DIRECTORY}/index.ejs`,
   filename: 'index.html',
   inject: 'body',
 })
 
-
 const configuration = {
-  entry: APP_DIRECTORY + '/index.js',
+
+  context: APP_DIRECTORY,
+
+  entry: {
+    app: [
+      `webpack-dev-server/client?http://localhost:${PORT}`,
+      `${APP_DIRECTORY}/index.js`,
+    ],
+
+  },
 
   output: {
     path: BUILD_DIRECTORY,
+    publicPath: BUILD_DIRECTORY,
     filename: '[name].bundle.js',
   },
 
@@ -30,6 +40,10 @@ const configuration = {
 
   devServer: {
     port: PORT,
+    inline: false,
+    contentBase: BUILD_DIRECTORY,
+    publicPath: BUILD_DIRECTORY,
+    outputPath: BUILD_DIRECTORY,
   },
 
   module: {
