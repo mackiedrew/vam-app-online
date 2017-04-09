02c7c22761e5e7b0a7650c0bae93dd320e1819ee
Mackenzie Drew <mackiedrew@gmail.com>
2017-04-09 11:14:30 -0400

    :white_check_mark: Hopefully impropving coverage
    - I think I'll need to break this stuff into smaller functions
    to properly test them.

--- src/libraries/wavHelp.test.js
+++ src/libraries/wavHelp.test.js
@@ -1,4 +1,4 @@
-import { secondsToSamples, readWav } from './wavHelp'
+import { secondsToSamples, readWav, logWav, richReadWav } from './wavHelp'
 
 describe('secondsToSamples(seconds, sampleRate)', () => {
 
@@ -19,25 +19,25 @@ describe('secondsToSamples(seconds, sampleRate)', () => {
 
 })
 
+const badSampleWavPath = '/XYZ123/ThisFileDoesNotExist.zip'
+const sampleWavPath = './example/sample.wav'
+
 describe('readWav(filePath)', () => {
   
   it('promise is rejected if the file does not exist', () => {
-    const expectedResponse ='ERROR'
-    const response = readWav('/XYZ123/ThisFileDoesNotExist.zip')
+    const response = readWav(badSampleWavPath)
     .then(() => assume(true).to.be.equal(false))
     .catch(() => assume(true).to.be.equal(true))
   })
 
   it('promise is resolved if the file is a valid wav', () => {
-    const expectedResponse ='ERROR'
-    const response = readWav('./example/sample.wav')
+    const response = readWav(sampleWavPath)
     .then(() => assume(true).to.be.equal(true))
     .catch(() => assume(true).to.be.equal(false))
   })
 
   it('returns a buffer containing sampleRate and an array of channels', () => {
-    const expectedResponse ='ERROR'
-    const response = readWav('./example/sample.wav')
+    const response = readWav(sampleWavPath)
     .then((buffer) => {
       assume(buffer.sampleRate).to.be.above(0)
       assume(channelData.length).to.be.above(0)
@@ -47,9 +47,53 @@ describe('readWav(filePath)', () => {
 })
 
 describe('logWav(filePath)', () => {
-  it('works')
+  it('returns nothing', () => {
+    const result = logWav(sampleWavPath)
+    assume(result).to.be.equal(undefined)
+  })
 })
 
 describe('richReadWav(filePath)', () => {
-  it('works')
+  
+  it('returns a promise', () => {
+    assume(richReadWav(sampleWavPath)).to.be.a('promise')
+  })
+
+  it('resolves an object from promise', () => {
+    richReadWav(sampleWavPath)
+    .then((result) => assume(result).to.be.an('object'))
+  })
+
+  it('returns an object with a reasonable sample rate', () => {
+    richReadWav(sampleWavPath)
+    .then(({sampleRate}) => {
+      assume(sampleRate).to.be.a('number')
+      assume(sampleRate).to.be.above(0)
+      assume(sampleRate).to.be.below(9999999999)
+    })
+  })
+
+  it('returns an object with a reasonable length', () => {
+    richReadWav(sampleWavPath)
+    .then(({length}) => {
+      assume(length).to.be.a('number')
+      assume(length).to.be.above(0)
+    })
+  })
+
+  it('returns an object with a reasonable number of grains', () => {
+    richReadWav(sampleWavPath)
+    .then(({grains}) => {
+      assume(grains).to.be.an('array')
+      assume(grains.length).to.be.above(0)
+    })
+  })
+
+  it('returns an object with a reasonable maximum amplitude', () => {
+    richReadWav(sampleWavPath)
+    .then(({maxAmplitude}) => {
+      assume(maxAmplitude).to.be.a('number')
+      assume(maxAmplitude).to.be.above(0)
+    })
+  })
 })
