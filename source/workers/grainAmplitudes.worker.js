c9d923974feb51ab78e38ad86229ebb8d361ad77
Mackenzie Drew <mackiedrew@gmail.com>
2017-04-29 13:31:17 -0400

    :rotating_light: Re-enabled linting on workers
    Also fixed the errors

--- source/workers/grainAmplitudes.worker.js
+++ source/workers/grainAmplitudes.worker.js
@@ -1,20 +1,17 @@
-/* eslint-disable */
-
 import registerPromiseWorker from "promise-worker/register";
 import { mean, zipObjectArray, max } from "../help/generic/generic";
 import { areGrainsQuiet } from "../help/grain/grain";
 import config from "../config";
 
 registerPromiseWorker(({ protoGrains, cases }) => {
-  
   // Add amplitudes to grains
   const amplitudes = cases.map(mean);
   const simpleGrains = zipObjectArray(protoGrains, "amplitude", amplitudes);
 
   // Add quietness to grains
-  const quietnessCutoff =  config.quietCutoff.value / 100;
+  const quietnessCutoff = config.quietCutoff.value / 100;
   const quietGrains = areGrainsQuiet(simpleGrains, quietnessCutoff);
-  const finalGrains = zipObjectArray(simpleGrains, "quiet", quietGrains); 
+  const finalGrains = zipObjectArray(simpleGrains, "quiet", quietGrains);
 
   // Calculate max amplitude
   const maxAmplitude = max(amplitudes);
