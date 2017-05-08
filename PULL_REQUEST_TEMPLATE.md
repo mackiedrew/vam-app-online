25721cb23594e585f73428a3814b027dc7d89844
Mackenzie Drew <mackiedrew@gmail.com>
2017-05-08 14:19:24 -0400

    :bug: Fixed issue with removing selected track leaving no tracks selected

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,14 +1,21 @@
-<!--- Provide a general summary of your changes in the Title above -->
+## Change Summary
 
-## Description
-<!--- Describe your changes in detail -->
 
-## Checklist:
-<!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
-<!--- If you're unsure about any of these, don't hesitate to ask. We're here to help! -->
-- [ ] All added user controls can be easily accessed through the keyboard only.
+## Contributor Checklist:
 - [ ] My code passes linting for both Stylus and JavaScript.
 - [ ] I have updated the documentation accordingly.
 - [ ] I have added tests to cover my changes.
-- [ ] All new and existing tests passed.
+- [ ] Keyboard controls added for all new features
+- [ ] Code has been reviewed.
+- [ ] Code passes all checks.
+- [ ] If applicable, new code is covered by flow.
 
+## Reviewer Checklist:
+- [ ] Is it documented enough well enough to be easily understood
+- [ ] Do unit tests cover edge cases of new code that coverage can't find?
+- [ ] The changes are actually reflected in a locally run version.
+- [ ] No spelling or grammar issues.
+- [ ] Identifier names are descriptive and verbose without adding confusion.
+- [ ] Is there a better way to accomplish the new change?
+- [ ] Is there any redundant or duplicate code (other than tests)?
+- [ ] Are possible errors and edge cases handled?
--- source/actions/removeTrack.js
+++ source/actions/removeTrack.js
@@ -1,32 +1,64 @@
+// @flow
+
+// Flow Types
+import type { ReduxAction } from "../constants/flowTypes";
+
 // Action Type
 import { REMOVE_TRACK } from "../constants/actionTypes";
 
+// Actions
+import selectTrack from "./selectTrack";
+
+// Selectors
+import trackIdArray from "../selectors/trackIdArray";
+
+// Helpers
+import { clamp } from "../help/generic";
+
 /**
- * Action creator: creates an action that sets a new track list to delete a track.
+ * Action creator: creates an action that sets a new track list to delete a
+ * track.
  * 
- * @param {Object} newTrackList An entire new track list to swap in for the old one.
- * @returns {Object} Action: removes track by setting the track list to a new object.
+ * @param {Object} newTrackList An entire new track list to swap in for the old
+ * one.
+ * @returns {Object} Action: removes track by setting the track list to a new
+ * object.
  */
-export const removeTrackSimple = newTrackList => {
+export const removeTrackSimple = (newTrackList: Object): ReduxAction => {
   return { type: REMOVE_TRACK, payload: newTrackList };
 };
 
 /**
  * Thunk: removes a track from the track list.
  * 
- * @param {Object} trackId Which track to remove by ID.
+ * @param {string} trackId Which track to remove by ID.
  * @returns {Function} Action creator that removes a track from the track list. 
  */
-const removeTrack = trackId => {
-  return (dispatch, getState) => {
-    const { trackList } = getState().tracks;
-    const trackIds = Object.keys(trackList);
+const removeTrack = (trackId: string) => {
+  return (dispatch: Function, getState: Function) => {
+    // Get a list of existing tracks.
+    const state = getState();
+    const trackIds = trackIdArray(state);
+    const { trackList, selectedTrack } = state.tracks;
+
+    // Reconstruct a whole new track list to avoid mutation.
     const filteredIds = trackIds.filter(id => trackId !== id);
     const newTrackList = filteredIds.reduce(
       (current, id) => ({ ...current, [id]: trackList[id] }),
       {}
     );
     dispatch(removeTrackSimple(newTrackList));
+
+    if (trackId === selectedTrack) {
+      // Find the next track that should be selected after track is removed.
+      const trackListLength: number = trackIds.length;
+      const oldMaxIndex: number = trackListLength - 1;
+      const newMaxIndex: number = oldMaxIndex - 1;
+      const removedTrackIndex: number = trackIds.indexOf(trackId);
+      const nextTrackId: number = clamp(removedTrackIndex, 0, newMaxIndex);
+      const nextTrack: string = filteredIds[nextTrackId];
+      dispatch(selectTrack(nextTrack));
+    }
   };
 };
 
